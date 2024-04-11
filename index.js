f96d2141238eea15ca2a609a761cc4c4397e8ddd
Aashka Chhabra <157134139+Aashka19@users.noreply.github.com>
2024-04-12 00:16:19 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import Connection from './Database/db.js';
-
-const  app= express();
-const PORT=8000;
-Connection();
-app.listen(PORT,()=> console.log(`server is running succesfully on PORT ${PORT}`))
