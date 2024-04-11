18b894bc14cc76ef73aba3c8e7ca0e50ddc67cac
NamitaYGit <147243906+NamitaYGit@users.noreply.github.com>
2024-04-11 20:05:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import express from 'express';
+import Connection from './Database/db.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const  app= express();
+const PORT=8000;
+Connection();
+app.listen(PORT,()=> console.log(`server is running succesfully on PORT ${PORT}`))
